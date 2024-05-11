8281f828d1f632f620694c76a6b5cd9b9e21987d
praveenkumar882003 <164858953+praveenkumar882003@users.noreply.github.com>
2024-05-11 14:08:21 +0530

    Exp2_Encoder8to3.v

--- Exp2_Encoder8to3.v
+++ Exp2_Encoder8to3.v
@@ -1,7 +1,8 @@
-module encoder_8(d,a,b,c);
-input [7:0]d;
-output a,b,c;
 
-
-
+module encoder_8_to_3(a0,a1,a2,d0,d1,d2,d3,d4,d5,d6,d7);
+input d0,d1,d2,d3,d4,d5,d6,d7;
+output a0,a1,a2;
+assign a0 = ( d1 | d3 | d5 | d7 );
+assign a1 = ( d2 | d3 | d6 | d7 );
+assign a2 = ( d4 | d6 | d5 | d7 );
 endmodule
